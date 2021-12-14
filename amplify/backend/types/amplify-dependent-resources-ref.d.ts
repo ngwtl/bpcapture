c49f7b985098cbbeac6dda20a61ae53b0b461ada
Ng, Leonard (Manufacturing, Clayton North) <Weitatleonard.Ng@csiro.au>
2021-12-14 22:40:48 +1100

    add amplify directory

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "bpcapture": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
